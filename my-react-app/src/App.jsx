ccb0c28d9f9a8bdd2b660d6b9d3895b965a0fed2
olamide <olamideashaolu318.gmail.com>
2025-10-20 22:35:39 +0100

    enabled deployement with GH pages

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,35 +1,49 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+// import './App.css'
+import './index.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+export function Card({name, title, about}){
+  return(
+     <div className='card'>
+      <h2>{name}</h2>
+      <p className='card-title'>{title}</p>
+      <h3 className='second-title'>{about}</h3>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
-export default App
+export default function App(){
+   const profiles = [
+        {id:1 , 
+        name:"Jason Alexander", 
+        title:"Marine Engineer", 
+        about:"I’m a Marine Engineer specializing in ship systems, propulsion, and sustainable maritime solutions."},
+        {id:2, 
+        name:"Muhhamed ali", 
+        title:"Lawyer", 
+        about:"I like solving legal problems, helping clients find justice, and debating complex legal issues."},
+        {id:3 , 
+        name:"Olivia Chen", 
+        title:"Educator", 
+        about:"I love inspiring students, sharing knowledge, and helping others reach their full potential."},
+        {id:4 , 
+        name:"Aramide Olabisi", 
+        title:"AI Engineer", 
+        about:"I love building intelligent systems, training models, and exploring how AI can solve real problems."}
+      ];
+
+  return(
+    <div className='holder'>
+    {profiles.map(profile => (
+      <Card
+       key={profile.id}
+       name={profile.name}
+       title={profile.title}
+       about={profile.about}
+      />
+    ))}
+    </div>
+  )
+}
